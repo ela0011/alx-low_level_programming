986a1358867e1b495fc7b8c0ea0fec4088762667
eliyas <eliyasshafi033@gmail.com>
2022-12-08 11:37:27 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 /**
-*main - prints exactly and that piece of art is useful- Dora Korpar, 2015-10-19,
-*
-*
-*Return: Always 0 (Success)
-**/
+*main - prints exactly and that piece of art is useful
+*Return: Always 1 (Success)
+*/
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(STDOUT_FILENO, "and that piece of art is useful
+							\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
